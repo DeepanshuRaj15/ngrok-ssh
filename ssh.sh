1de334aff595fa6b7c096dfc4fe512bce6e17c3f
DeepanshuRaj15 <62454655+DeepanshuRaj15@users.noreply.github.com>
2022-01-08 07:56:54 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
 echo "Decompression"
+ls
 unzip ngrok.zip
 rm ngrok.zip
 ./ngrok authtoken 1aErt4ZiVM3wBwVIoBdKvw6BYxN_2jNxExRWoZJe8K3tzidJC
 rm /etc/ssh/sshd_config
 wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/DeepanshuRaj15/ngrok-ssh/main/sshd_config
+
 service ssh restart
 ./ngrok tcp 22
